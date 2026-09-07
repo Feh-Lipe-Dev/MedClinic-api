0b2c172c392ea1ba97cd8368609fba6072f05db5
Felipe Goncalves <felipe.cleia05@gmail.com>
2026-09-07 14:42:10 -0300

    feat: configurada conexão com o PostgreSQL utilizando TypeORM

---
+++ src/data-source.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata'
+import dotenv from 'dotenv'
+import { DataSource } from 'typeorm'
+
+dotenv.config()
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl:
+        process.env.DB_SSL === "true"
+            ? { rejectUnauthorized: false }
+            : false,
+    synchronize: true,
+    logging: false,
+    entities: []
+})
